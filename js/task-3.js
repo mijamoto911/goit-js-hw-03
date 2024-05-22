9a68bca38ebdcc6c8e6c8ec352952e498ff6b1df
Eduard Schumacher <107512513+mijamoto911@users.noreply.github.com>
2024-05-22 15:47:51 +0200

    hw-3

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+function filterArray(numbers, value) {
+  const numResult = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      numResult.push(numbers[i]);
+    }
+  }
+  return numResult;
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
